f48f3ffa57403d32f20579e958fd34c04894deda
Muqeem Ahmed <u2023561@giki.edu.pk>
2026-04-04 04:07:56 +0500

    Added/ connected git user to shell file

--- app/scripts/user_data.sh
+++ app/scripts/user_data.sh
@@ -6,7 +6,7 @@ pip3 install flask requests boto3
 
 # Clone the repository
 cd /home/ec2-user
-git clone https://github.com/[YOUR_USERNAME]/Assignment1_AWS_CE.git
+git clone https://github.com/Muqeem-Ahmed/Assignment1_AWS_CE.git
 cd Assignment1_AWS_CE/app
 
 # Set environment variables for S3
